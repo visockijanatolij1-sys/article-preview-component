5889a5485e20f06af3dffaad299eb64ea4bb98b4
Анатолій <visockijanatolij1@gmail.com>
2026-06-06 19:21:27 +0200

    update popup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+
 const shareButton = document.querySelector(".share-button");
 const sharePopup = document.querySelector(".share-popup");
+const articleFooter = document.querySelector(".artilce-preview-block");
 
 shareButton.addEventListener("click", () => {
-    sharePopup.classList.toggle("active");
+
+    if (window.innerWidth <= 767) {
+        articleFooter.classList.toggle("active");
+    } else {
+        sharePopup.classList.toggle("active");
+    }
+
 });
